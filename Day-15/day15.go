6905e0e5c0636efc82cfd1611cadd570a2c8abb4
Natalie Girard <girardnatalie@gmail.com>
2019-12-15 22:36:24 -0500

    Day 15 part 1 solution

---
+++ Day-15/day15.go
@@ -0,0 +1,325 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type coordinate struct {
+	x, y int
+}
+
+type cell struct {
+	maze  string
+	steps int
+}
+
+func convertInput(input string) []int64 {
+	arr := strings.Split(input, ",")
+
+	values := make([]int64, 100000)
+	for i := 0; i < len(arr); i++ {
+		values[i], _ = strconv.ParseInt(arr[i], 10, 64)
+	}
+	return values
+}
+
+func parseInstruction(code int64) (int64, int64, int64, int64) {
+	opcode := code % 100
+	mode1 := (code / 100) % 10
+	mode2 := (code / 1000) % 10
+	mode3 := (code / 10000) % 10
+
+	return opcode, mode1, mode2, mode3
+}
+
+func getValue(values []int64, mode int64, index int64, relativeBase int64) int64 {
+	if mode == 1 {
+		return index
+	}
+	if mode == 2 {
+		return values[index+relativeBase]
+	}
+	return values[index]
+}
+
+func Intcode(values []int64, message chan int64) int64 {
+	var i int64 = 0
+	var lastOutput int64 = 0
+	var relativeBase int64 = 0
+	endloop := false
+	for {
+		opcode, mode1, mode2, mode3 := parseInstruction(values[i])
+		//fmt.Println(i, opcode)
+		switch opcode {
+		case 1:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			index3 := values[i+3]
+			if mode3 == 2 {
+				index3 += relativeBase
+			}
+			values[index3] = value1 + value2
+			i += 4
+		case 2:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			index3 := values[i+3]
+			if mode3 == 2 {
+				index3 += relativeBase
+			}
+			values[index3] = value1 * value2
+			i += 4
+		case 3:
+			index := values[i+1]
+			inputValue := <-message
+			if mode1 == 2 {
+				index += relativeBase
+			}
+			values[index] = inputValue
+			i += 2
+		case 4:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			fmt.Println("Output:", value1)
+			lastOutput = value1
+			message <- value1
+			i += 2
+		case 5:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			if value1 != 0 {
+				i = value2
+			} else {
+				i += 3
+			}
+		case 6:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			if value1 == 0 {
+				i = value2
+			} else {
+				i += 3
+			}
+		case 7:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			index3 := values[i+3]
+			if mode3 == 2 {
+				index3 += relativeBase
+			}
+
+			if value1 < value2 {
+				values[index3] = 1
+			} else {
+				values[index3] = 0
+			}
+			i += 4
+		case 8:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			value2 := getValue(values, mode2, values[i+2], relativeBase)
+			index3 := values[i+3]
+			if mode3 == 2 {
+				index3 += relativeBase
+			}
+
+			if value1 == value2 {
+				values[index3] = 1
+			} else {
+				values[index3] = 0
+			}
+			i += 4
+		case 9:
+			value1 := getValue(values, mode1, values[i+1], relativeBase)
+			relativeBase += value1
+			i += 2
+		case 99:
+			close(message)
+			<-message
+			endloop = true
+		}
+
+		if endloop {
+			time.Sleep(100 * time.Millisecond)
+			break
+		}
+	}
+	return lastOutput
+}
+
+func find(a []int, x int) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return len(a)
+}
+
+func moveInDirection(coord coordinate, direction int) coordinate {
+	newCoord := coord
+
+	switch direction {
+	case 1:
+		newCoord.y++
+	case 2:
+		newCoord.y--
+	case 3:
+		newCoord.x--
+	case 4:
+		newCoord.x++
+	}
+
+	return newCoord
+}
+
+func getNewDirection(area map[coordinate]cell, curr coordinate, direction int) (int, coordinate) {
+	moves := []int{1, 4, 2, 3}
+	dir := find(moves, direction)
+	dir = (dir + 1) % 4 // try to move to the right
+	coord := moveInDirection(curr, moves[dir])
+
+	i := 0
+	for area[coord].maze == "#" && i < 4 {
+		fmt.Println(area[coord], coord, moves[dir], "dir", dir)
+		dir = (dir - 1) % 4
+		if dir < 0 {
+			dir = dir + 4
+		}
+		fmt.Println("dir:", dir)
+		coord = moveInDirection(curr, moves[dir])
+		i++
+	}
+	fmt.Println("New direction", moves[dir], coord, "'", area[coord], "'")
+	return moves[dir], coord
+}
+
+func printArea(area map[coordinate]cell) {
+	minX, maxX, minY, maxY := 0, 0, 0, 0
+	for key := range area {
+		if key.x < minX {
+			minX = key.x
+		}
+		if key.x > maxX {
+			maxX = key.x
+		}
+		if key.y < minY {
+			minY = key.y
+		}
+		if key.y > maxY {
+			maxY = key.y
+		}
+	}
+
+	room := make([][]string, maxY-minY+1)
+	for i := 0; i < maxY-minY+1; i++ {
+		room[i] = make([]string, maxX-minX+1)
+	}
+
+	for key, value := range area {
+		if value.maze == "#" {
+			room[key.y-minY][key.x-minX] = value.maze
+		} else {
+			room[key.y-minY][key.x-minX] = strconv.Itoa(value.steps)
+		}
+	}
+
+	f, _ := os.Create("output.csv")
+	defer f.Close()
+	for i := 0; i < maxY-minY+1; i++ {
+		comma := strings.Join(room[i], ",")
+		fmt.Fprintln(f, comma)
+	}
+}
+
+func main() {
+	b, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	str = strings.Replace(str, "\n", "", -1)
+	values := convertInput(str)
+
+	message := make(chan int64)
+	go Intcode(values, message)
+
+	area := make(map[coordinate]cell)
+	output := 1
+	var curr coordinate
+	direction := 0
+	i := 0
+	for output != 2 {
+		fmt.Println("(", i, ")", output, "in direction", direction, curr)
+		//store map information based on received output
+		var c cell
+		c.steps = -1
+		switch direction {
+		case 1:
+			if output == 0 {
+				c.maze = "#"
+				area[curr] = c
+				curr.y--
+				direction = 2
+			} else {
+				c.maze = "."
+				area[curr] = c
+			}
+		case 2:
+			if output == 0 {
+				c.maze = "#"
+				area[curr] = c
+				curr.y++
+				direction = 1
+			} else {
+				c.maze = "."
+				area[curr] = c
+			}
+		case 3:
+			if output == 0 {
+				c.maze = "#"
+				area[curr] = c
+				curr.x++
+				direction = 4
+			} else {
+				c.maze = "."
+				area[curr] = c
+			}
+		case 4:
+			if output == 0 {
+				c.maze = "#"
+				area[curr] = c
+				curr.x--
+				direction = 3
+			} else {
+				c.maze = "."
+				area[curr] = c
+			}
+		default:
+			if output == 1 && i == 0 {
+				c.maze = "o"
+				area[curr] = c
+			} else {
+				c.maze = "."
+				area[curr] = c
+			}
+		}
+
+		newDir, coord := getNewDirection(area, curr, direction)
+		curr = coord
+		direction = newDir
+		message <- int64(newDir)
+
+		receive := <-message
+		output = int(receive)
+		i++
+	}
+
+	c := cell{"*", 0}
+	area[curr] = c
+	printArea(area) // part 1: 300
+
+}
