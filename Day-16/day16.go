c753cda29f9772906722d7f420847fadb313c33f
Natalie Girard <girardnatalie@gmail.com>
2019-12-16 17:56:37 -0500

    Day 16 part 1 solution

---
+++ Day-16/day16.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func getPattern(basePattern []int, index int, size int) []int {
+	pattern := make([]int, size)
+
+	count := 0 // number of times the basePattern pos was used
+	pos := 0   // position in the basePattern
+	i := 0
+	first := true
+	for {
+		if i == size {
+			break
+		}
+		repeats := index + 1
+		if first {
+			first = false
+			count++
+			continue
+		}
+
+		if count == repeats {
+			pos = (pos + 1) % len(basePattern)
+			count = 0
+		} else {
+			pattern[i] = basePattern[pos]
+			count++
+			i++
+		}
+	}
+
+	return pattern
+}
+
+func getNewValue(pattern []int, signal []int) int {
+	val := 0
+	for i := 0; i < len(signal); i++ {
+		val += (pattern[i] * signal[i])
+	}
+
+	val = val % 10
+	if val < 0 {
+		val *= -1
+	}
+	return val
+}
+
+func main() {
+	b, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	str = strings.TrimSuffix(str, "\n")
+
+	inputSignal := strings.Split(str, "")
+	signal := make([]int, len(inputSignal))
+	for i := 0; i < len(inputSignal); i++ {
+		signal[i], _ = strconv.Atoi(inputSignal[i])
+	}
+
+	basePattern := []int{0, 1, 0, -1}
+	workingSignal := make([]int, len(signal))
+
+	for a := 0; a < 100; a++ {
+		for i := 0; i < len(signal); i++ {
+			pattern := getPattern(basePattern, i, len(signal))
+			workingSignal[i] = getNewValue(pattern, signal)
+		}
+		signal = workingSignal
+	}
+	fmt.Println("last phase", signal[:8]) // Part 1: 40580215
+}
